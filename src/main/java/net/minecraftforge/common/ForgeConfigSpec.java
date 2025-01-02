bc3213242ea6401d3cfb0f23e3b28fbfea8c554b
Jonathing <me@jonathing.me>
2025-01-01 20:19:56 -0500

    Access to config spec's line joiner

--- src/main/java/net/minecraftforge/common/ForgeConfigSpec.java
+++ src/main/java/net/minecraftforge/common/ForgeConfigSpec.java
@@ -956,7 +956,7 @@ public class ForgeConfigSpec extends UnmodifiableConfigWrapper<UnmodifiableConfi
         }
     }
 
-    private static final Joiner LINE_JOINER = Joiner.on("\n");
+    public static final Joiner LINE_JOINER = Joiner.on("\n");
     public static final Joiner DOT_JOINER = Joiner.on(".");
     private static final Splitter DOT_SPLITTER = Splitter.on(".");
     private static List<String> split(String path)
